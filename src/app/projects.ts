1451b0afa9f9b36e6710aa835bd7077317d096b3
peleyal <peleyal@google.com>
2020-05-12 18:38:26 -0700

    s/product/project

---
+++ src/app/projects.ts
@@ -0,0 +1,21 @@
+export const projects = [
+  {
+    id: 'csp-cohero-prod-data',
+  },
+  {
+    id: 'csp-coreg-prod-data',
+  },
+  {
+    id: 'csp-shasta-prod-data',
+  },
+  {
+    id: 'csp-cohero-nonprod-data',
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
